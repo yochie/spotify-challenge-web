ad237ea9503a0619630ced887dbf51b0c67f9b26
yoann <yoann.ochietti@gmail.com>
2024-08-05 15:09:34 -0400

    login cookie lifetime extended

--- SpotifyPlaylisterApp/Program.cs
+++ SpotifyPlaylisterApp/Program.cs
@@ -85,7 +85,7 @@ builder.Services.ConfigureApplicationCookie(options =>
 {
     // Cookie settings
     options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
 
     options.LoginPath = "/Identity/Account/Login";
     options.AccessDeniedPath = "/Identity/Account/AccessDenied";
